7ac391645bed6e7620243213be557d8a4bb16eb2
1812 <francogadea1812@gmail.com>
2022-11-11 18:27:13 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+//? Bootstrap y Bootstrap icons
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
