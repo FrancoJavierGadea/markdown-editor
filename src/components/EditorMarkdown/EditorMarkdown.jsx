47c2a307b0eddcfddb217057758be80ae6924deb
1812 <francogadea1812@gmail.com>
2022-11-17 05:47:27 -0300

    Agregando al Editor la posibilidad de usar tabulacion

--- src/components/EditorMarkdown/EditorMarkdown.jsx
+++ src/components/EditorMarkdown/EditorMarkdown.jsx
@@ -6,12 +6,34 @@ function EditorMarkdown({value, onChange, style}) {
 
     const change = ({target: {value}}) => {
 
+        console.log('change');
+
         onChange(value);
+    }
+
+    const tabulation = ({key, nativeEvent, target}) => {
+
+        //* docs: https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+
+        if(key === 'Tab'){
+
+            nativeEvent.preventDefault();
+            
+            //? caret position
+            let start = target.selectionStart;
+            let end = target.selectionEnd;
+            
+            //? Set textarea value to: text before caret + tab + text after caret
+            target.value = target.value.substring(0, start) + "\t" + target.value.substring(end);
+
+            //? Put caret at right position again
+            target.selectionEnd = start + 1;
+        }
     }
 
     return (<div className="EditorMarkdown py-2">
 
-        <Form.Control as="textarea" style={{overflow: 'visible', resize: 'none', cursor: 'initial', ...style}} defaultValue={value} onKeyUp={change} />
+        <Form.Control as="textarea" style={{overflow: 'visible', resize: 'none', cursor: 'text', ...style}} defaultValue={value} onKeyUp={change} onKeyDown={tabulation} />
 
     </div>);
 }
